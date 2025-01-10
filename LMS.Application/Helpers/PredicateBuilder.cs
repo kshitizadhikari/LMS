b7d74dc9ed7f8b383b42285e82d00522c50768c6
kshitiz <adhikarikshitiz12@gmail.com>
2025-01-10 16:39:19 +0545

    add PredicateBuilder

---
+++ LMS.Application/Helpers/PredicateBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+public static class PredicateBuilder
+{
+    public static Expression<Func<T, bool>> True<T>() => param => true;
+    public static Expression<Func<T, bool>> False<T>() => param => false;
+
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+
+        var body = Expression.AndAlso(
+            Expression.Invoke(expr1, parameter),
+            Expression.Invoke(expr2, parameter)
+        );
+
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+
+        var body = Expression.OrElse(
+            Expression.Invoke(expr1, parameter),
+            Expression.Invoke(expr2, parameter)
+        );
+
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+}
